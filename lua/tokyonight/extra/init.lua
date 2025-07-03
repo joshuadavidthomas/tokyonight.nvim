75602dce800b9bdd996893e64a984f3efb1bebd1
Josh <josh@joshthomas.dev>
2025-07-02 21:39:22 -0500

    feat(extra): add themes for hyprland

--- lua/tokyonight/extra/init.lua
+++ lua/tokyonight/extra/init.lua
@@ -20,6 +20,7 @@ M.extras = {
   gitui            = { ext = "ron", url = "https://github.com/extrawurst/gitui", label = "GitUI" },
   gnome_terminal   = { ext = "dconf", url = "https://gitlab.gnome.org/GNOME/gnome-terminal", label = "GNOME Terminal" },
   helix            = { ext = "toml", url = "https://helix-editor.com/", label = "Helix" },
+  hyprland         = { ext = "conf", url = "https://hypr.land", label = "Hyprland" },
   iterm            = { ext = "itermcolors", url = "https://iterm2.com/", label = "iTerm" },
   kitty            = { ext = "conf", url = "https://sw.kovidgoyal.net/kitty/conf.html", label = "Kitty" },
   lazygit          = { ext = "yml", url = "https://github.com/jesseduffield/lazygit", label = "Lazygit" },
